d72d75c9788e0a5e32237df00bfad5b6bc7605a1
Ajeya Nayak <ajeyanayak@gmail.com>
2021-01-02 12:28:57 +0530

    Custom MOdule - Situation 3

--- custom_module/situation_3/source/main.py
+++ custom_module/situation_3/source/main.py
@@ -1,18 +1,24 @@
 ''' importing from customised file'''
+import sys
+temp_list = str(__file__).split("/")
+temp_join = "/".join(temp_list[:-1])
+temp_con = (temp_join + "/../libraries")
+sys.path.append(temp_con)
+
 from operation import add_num
 from operation import sub_num
 from operation import mul_num
 from operation import div_num
 from operation import odd_even
-from helper import display_name
 from helper import display_greeting
+from helper import display_name
 
-''' This function will greets with your entered name and do the mathematical operations'''
+# This function will greets with your entered name and do the mathematical librariess
 def oper_num():
     display_greeting()
     name_2 = input("Please Enter Your name:")
     display_name(name_2)
-    print("Now we are going to start Mathematical Operations")
+    print("Now we are going to start Mathematical librariess")
     value_1 = int(input("Please Enter a Number:"))
     value_2 = int(input("Please Enter another number:"))
     add_num(value_1,value_2)
@@ -27,4 +33,3 @@ def oper_num():
 
 if __name__ == '__main__':
     oper_num()
-
