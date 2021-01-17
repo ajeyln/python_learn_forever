98a611cd44c90fa701d13f85c63d27987270ec65
Ajeya Nayak <ajeyanayak@gmail.com>
2021-01-17 17:44:59 +0530

    Logging

---
+++ logging/excercises - 2/main.py
@@ -0,0 +1,79 @@
+''' In order to import custom file operation.py and to do mathematical operation'''
+import sys
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# file handler for Debug
+formatter_debug = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler_debug = logging.FileHandler('main.log')
+file_handler_debug.setLevel(logging.DEBUG)
+file_handler_debug.setFormatter(formatter_debug)
+
+logger.addHandler(file_handler_debug)
+
+# Stream handler
+stream_data = logging.StreamHandler()
+stream_data.setFormatter(formatter_debug)
+
+logger.addHandler(stream_data)
+
+# file handler for Info
+formatter_info = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s')
+
+file_handler_info = logging.FileHandler('main.log')
+file_handler_info.setLevel(logging.INFO)
+file_handler_info.setFormatter(formatter_info)
+
+logger.addHandler(file_handler_info)
+
+# file Handler Critical
+formatter_critical = logging.Formatter('%(asctime)s:%(message)s:%(name)s')
+
+file_handler_critical = logging.FileHandler('main.log')
+file_handler_critical.setLevel(logging.CRITICAL)
+file_handler_critical.setFormatter(formatter_critical)
+
+logger.addHandler(file_handler_critical)
+
+#pylint: disable=wrong-import-position
+#pylint: disable=import-error
+
+from operation import add_num
+from operation import sub_num
+from operation import mul_num
+from operation import div_num
+from operation import odd_even
+
+#pylint: enable=wrong-import-position
+#pylint: enable=import-error
+
+def oper_num(value_1, value_2):
+    ''' In order to import custom files from libraries folder,
+    we need to reach libraries folder from source folder'''
+    logger.critical(f"The first number is {value_1}")
+    logger.critical(f"The second number is {value_2}")
+    x = add_num(value_1,value_2)
+    logger.info(f"addition of two number is {x}")
+    y = sub_num(value_1,value_2)
+    logger.info(f"substraction of two number is {y}")
+    z = mul_num(value_1,value_2)
+    logger.info(f"Multiplication of two number is {z}")
+    if value_2 == 0: # The condition will execute while second is zero
+        logger.debug("Zero Division error")
+    else:
+        a1 = div_num(value_1,value_2)
+        logger.debug(a1)
+    logger.info(odd_even(value_2))
+
+if __name__ == '__main__':
+    logger.info("Now we are doing mathematical operations")
+    if len(sys.argv) != 2:
+        print("$ python main.py <number_1> <number_2> ")
+        sys.exit(1)
+    
+    number_1 = int(sys.argv[1])
+    number_2 = int(sys.argv[2])
+    oper_num(number_1, number_2)
---
+++ logging/excercises - 2/test_operation.py
@@ -0,0 +1,51 @@
+# The Scripts is to test the libraries.operation.py
+
+''' Set up for importing file'''
+import os
+import sys
+import pytest
+sys.path.append(os.getcwd())
+
+#pylint: disable=wrong-import-position
+#pylint: disable=import-error
+
+from libraries.operation import add_num
+from libraries.operation import sub_num
+from libraries.operation import div_num
+from libraries.operation import mul_num
+from libraries.operation import odd_even
+
+#pylint: enable=wrong-import-position
+#pylint: enable=import-error
+
+def test_add_num():
+    ''' to test add_num function in libraries.operation file'''
+    num_1 = 4
+    num_2 = 5
+    expected_value = 9
+    assert expected_value == add_num(num_1, num_2)
+
+def test_sub_num():
+    ''' to test sub_num function in libraries.operation file'''
+    num_3 = 12
+    num_4 = 3
+    expected_value = 9
+    assert expected_value == sub_num(num_3, num_4)
+
+def test_mul_num():
+    ''' to test mul_num function in libraries.operation file'''
+    try:
+        mul_num(10, 8)
+    except Exception as exc:
+        pytest.fail(f"Test is failed due to {exc}")
+
+
+@pytest.mark.parametrize("num_7, num_8, expected_value",[(9, 2, 4.5), (15, 0, None)])
+def test_div_num(num_7, num_8, expected_value):
+    ''' to test div_num function in libraries.operation file'''
+    assert expected_value == div_num(num_7, num_8)
+
+@pytest.mark.parametrize("num_11, expected_value",[(4, "even"),(7, "odd")])
+def test_odd_even(num_11, expected_value):
+    ''' to test odd_even function in libraries.operation file'''
+    assert expected_value == odd_even(num_11)
