bbf42887d7c0933eb8cf4e3c7cc89dda1612e048
Ajeya Nayak <ajeyanayak@gmail.com>
2021-03-19 00:59:26 +0530

    Programming CRUD Updated

--- database/programming_operation/CRUD_operation.py
+++ database/programming_operation/CRUD_operation.py
@@ -1,3 +1,4 @@
+''' this scripts has CRUD Operation on Database'''
 import sqlite3
 import os
 
@@ -9,41 +10,35 @@ conn = sqlite3.connect('school.db')
 c = conn.cursor()
 
 # Creating Table Student
-c.execute('''create table student(ID integer primary key, NAME varchar(15), AGE integer, SEX varchar(15), GRADE integer)''')
+c.execute('''create table student(ID integer primary key, NAME varchar(15), \
+            AGE integer, SEX varchar(15), GRADE integer)''')
 c.execute('insert into student values(2019101, "Amar", 6, "Male", 1)')
 c.execute('insert into student values(2019102, "Anil", 6, "Male", 1)')
 c.execute('insert into student values(2019103, "Anusha", 6, "Female", 1)')
 c.execute('insert into student values(2019104, "Akbar", 6, "Male", 1)')
 
 # Creating Table teacher
-c.execute('''create table teacher(ID varchar(8) primary key, NAME varchar(15), AGE integer, SEX varchar(15), SUBJECT varchar(15))''')
+c.execute('''create table teacher(ID varchar(8) primary key,\
+            NAME varchar(15), AGE integer, SEX varchar(15), SUBJECT varchar(15))''')
 c.execute('insert into teacher values("2015T01", "Kamala", 26, "Female", "Maths")')
 c.execute('insert into teacher values("2015T02", "Ajith Kumar", 32, "Male", "Science")')
 c.execute('insert into teacher values("2015T03", "Shivkumar", 33, "Male", "Social")')
 c.execute('insert into teacher values("2015T04", "Ranganath", 33, "Male", "Sports")')
 
-conn.commit()
-
-
 # Reading table Student
 c.execute("SELECT * FROM student")
 print(c.fetchall())
-
 
 # Reading table teacher
 c.execute("SELECT * FROM Student")
 print(c.fetchall())
 
-conn.commit()
-
 # Updataion
-c.execute('update teacher set NAME = "Ramesh" WHERE ID = "2015T04"') 
+c.execute('update teacher set NAME = "Ramesh" WHERE ID = "2015T04"')
 print("Upadated the table teacher, Changing the Name to Ramesh where ID = 2015T04")
 
 c.execute('update student set NAME = "Anthony" WHERE ID = 2019104')
 print("Upadated the table teacher, Changing the Name to Ramesh where ID = 2015T04")
-
-conn.commit()
 
 print("Reading table Student after updation")
 c.execute("SELECT * FROM student")
@@ -52,7 +47,3 @@ print(c.fetchall())
 print("Reading table teacher after updation")
 c.execute("SELECT * FROM teacher")
 print(c.fetchall())
-
-conn.commit()
-
-conn.close()
--- database/programming_operation/data_processing.py
+++ database/programming_operation/data_processing.py
@@ -1,3 +1,4 @@
+''' this script is to check the dataprocesing method in database'''
 import sqlite3
 import csv
 import os
@@ -6,12 +7,13 @@ def head_file(database_name, table_name, insert_file, update_file, delete_file):
     ''' Data processing methods'''
     cur_val = connect_database(database_name) # Connecting database
     list_header = get_header(cur_val, table_name) # Getting Header List
-    create_file('01_student_info_before_updation.csv', cur_val, table_name, list_header) # File creation before updation
-    insert_entry = insert_value(cur_val, insert_file) # Inserting values
+    create_file('01_student_info_before_updation.csv', cur_val, table_name,\
+                    list_header) # File creation before updation
+    insert_value(cur_val, insert_file) # Inserting values
     create_file('02_student_info_after_inserting.csv', cur_val, table_name, list_header)
-    update_entry = update_value(cur_val, list_header, table_name, update_file) # Updating value
+    update_value(cur_val, list_header, table_name, update_file) # Updating value
     create_file('03_student_info_after_updation.csv', cur_val, table_name, list_header)
-    delete_entry = delete_value(cur_val, table_name, list_header, delete_file) # Deleting values
+    delete_value(cur_val, table_name, list_header, delete_file) # Deleting values
     create_file('04_student_info_after_deletion.csv', cur_val, table_name, list_header)
 
 def connect_database(database_name):
@@ -30,10 +32,11 @@ def get_header(cur_val, table_name):
 def insert_value(cur_val, input_insert_file):
     '''Inserting values to table'''
     insert_line = create_input_list(input_insert_file)
-    insert_file = [b for b in insert_line]
+    insert_file = [b for b in insert_line] # pylint: disable=unnecessary-comprehension
     for i in range(1, len(insert_file)):
         cur_val.execute(f"insert into student values \
-            ({insert_file[i][0]}, \'{insert_file[i][1]}', {insert_file[i][2]}, \'{insert_file[i][3]}', {insert_file[i][4]})")
+            ({insert_file[i][0]}, \'{insert_file[i][1]}', {insert_file[i][2]}, \
+                \'{insert_file[i][3]}', {insert_file[i][4]})")
 
 def update_value(cur_val, list_header, table_name, input_update_file):
     '''Updating values to table'''
@@ -43,6 +46,7 @@ def update_value(cur_val, list_header, table_name, input_update_file):
         = \'{update_list_dict[1]['NAME']}' WHERE ID = {update_list_dict[1]['ID']};")
 
 def delete_value(cur_val, table_name, list_header, input_delete_file):
+    '''Deleting values from table'''
     delete_line = create_input_list(input_delete_file)
     delete_list_dict = create_list_dict(list_header, delete_line)
     cur_val.execute(f"DELETE FROM {table_name} WHERE ID = {delete_list_dict[1]['ID']}")
@@ -52,6 +56,7 @@ def create_table_list_dict(cur_val, table_name, list_header):
     cur_val.execute(f"Select * from {table_name}")
     list_values = [list(z) for z in cur_val.fetchall()]
     list_table_dicti = []
+    # pylint: disable=consider-using-enumerate
     for i in range(0, len(list_values)):
         dict_row = {}
         for j in range(0, len(list_values[0])):
@@ -63,6 +68,7 @@ def create_table_list_dict(cur_val, table_name, list_header):
 def create_list_dict(list_header, list_values):
     '''Creation of list of dictionaries from lists'''
     list_dicti = []
+    # pylint: disable=consider-using-enumerate
     for i in range(0, len(list_values)):
         dict_row = {}
         for j in range(0, len(list_values[0])):
@@ -81,12 +87,12 @@ def create_input_list(input_file):
 
 def create_file(file_name, cur_val, table_name, list_header):
     ''' Creation of files'''
-    if os.path.exists(f"result{os.path.sep}{file_name}"): # removing the files if already exists
+    if os.path.exists(f"result{os.path.sep}{file_name}"):
         os.remove(f"result{os.path.sep}{file_name}")
-        
     with open(f'result{os.path.sep}{file_name}', 'a', newline='') as file_details:
         writer = csv.writer(file_details)
         writer.writerow(list_header)
+        # pylint: disable=consider-using-enumerate
         list_dict = create_table_list_dict(cur_val, table_name, list_header)
         for k in range(0, len(list_dict)):
             print(list_dict[k].values())
